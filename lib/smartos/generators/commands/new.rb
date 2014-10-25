13f2603982ed2d0b9b4c49c1e710547f14dc656b
John Knott <john.knott@gmail.com>
2014-10-25 19:25:35 +0100

    More work on New command

--- lib/smartos/generators/commands/new.rb
+++ lib/smartos/generators/commands/new.rb
@@ -34,7 +34,7 @@ class New < SmartOS::Generators::Command
   def new_project(name)
     path = File.expand_path(name)
     if Dir.exists?(path)
-      puts "#{path} already exists. Please choose a directory name that doesn't already exist."
+      puts "#{path} already exists. Please choose a directory name that doesn't already exist.".red
       exit
     end      
 
@@ -44,35 +44,61 @@ class New < SmartOS::Generators::Command
 
     loop do
       new_global_zone  
-      break unless agree("Add another Global Zone?")
+      break unless agree("\nFinished configuring this Global Zone. Add another?")
     end
+
+    puts "\nYou have now configured your SmartOS virtual infrastructure. Inspect it, then run 'smartos up' to build it!".blue
   end
 
   def new_global_zone
     host_or_ip = info = nil
 
     loop do
-      host_or_ip = ask "Please enter the IP address or hostname of your SmartOS Global Zone:"
+      host_or_ip = ask "\nPlease enter the IP address or hostname of your SmartOS Global Zone:"
       info = SmartOS::GlobalZone.is_global_zone?(host_or_ip)
       break if info
+      puts "Not a valid SmartOS Global Zone hostname or IP address.".red
     end
 
-    puts "Successfully connected to Global Zone #{host_or_ip} (#{info})".green
+    puts "\nSuccessfully connected to Global Zone #{host_or_ip}".green
+    puts "#{info}\n".green
 
     # Gather information
     hostname_to_set = gather_hostname(host_or_ip)
     local_net_range = gather_pvn_vlan_details
+    internet_net_range = gather_internet_vlan_details
   end
 
   def gather_pvn_vlan_details
     loop do
-      answer = ask "Please enter the IP range you'd like to set up your Private Virtual Network in CIDR notation (e.g. 10.0.0.1/24)"
+      answer = ask "\nPlease enter the IP range you'd like to set up your Private Virtual Network in CIDR notation (e.g. 10.0.0.1/24)"
       begin
         ip = IPAddress.parse(answer)
         if ip.prefix == 32
-          puts "Please input a range. You inputed a single IP address."
-        break if 
+          puts "\nPlease enter a range. You entered a single IP address.".red
+        else
+          puts "\nConfiguring PVN as Address: #{ip.address} - Netmask: #{ip.netmask}".green
+          return ip
+        end
       rescue
+        puts "\nInvalid CIDR IP range.".red
+      end
+    end
+  end
+
+  def gather_internet_vlan_details
+    loop do
+      answer = ask "\nPlease enter the IP range you'd like to use for your Internet-facing Network in CIDR notation (e.g. 158.251.218.81/29)"
+      begin
+        ip = IPAddress.parse(answer)
+        if ip.prefix == 32
+          puts "\nPlease enter a range. You entered a single IP address.".red
+        else
+          puts "\nConfiguring internet-facing network as Address: #{ip.address} - Netmask: #{ip.netmask}".green
+          return ip
+        end
+      rescue
+        puts "\nInvalid CIDR IP range.".red
       end
     end
   end
