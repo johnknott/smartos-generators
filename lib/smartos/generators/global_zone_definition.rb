22477c16f095cf2ab417ba6a78ade3a4b962a9ab
John Knott <john.knott@gmail.com>
2014-11-04 01:29:34 +0000

    Shortened messy assignment.

--- lib/smartos/generators/global_zone_definition.rb
+++ lib/smartos/generators/global_zone_definition.rb
@@ -3,13 +3,10 @@ class GlobalZoneDefinition
                 :gz_pvn_ip, :gz_internet_ip, :vm_definitions 
 
   def initialize(gz_host, hostname, pvn_net_range, internet_net_range, dataset_repository, vm_definitions: [])
-
-    @gz_host, @hostname, @pvn_net_range, @internet_net_range, @dataset_repository = 
-      gz_host, hostname, pvn_net_range, internet_net_range, dataset_repository
+    method(__method__).parameters.each{ |arg| eval("@#{arg[1]}=#{arg[1]}") }
 
     @gz_pvn_ip = IPAddress.parse(@pvn_net_range.address)
     @gz_internet_ip = IPAddress.parse(@internet_net_range.address)
-    @vm_definitions = vm_definitions
   end
 
 end
--- lib/smartos/generators/machine_definition.rb
+++ lib/smartos/generators/machine_definition.rb
@@ -4,10 +4,7 @@ class MachineDefinition
 
   def initialize(dataset, machine_alias, hostname, pvn_ip,
                 internet_facing_ip: nil, memory_cap: nil, disk_cap: nil, cpu_cores: nil, copy_ssh_key: nil)
-
-    @dataset, @machine_alias, @hostname, @pvn_ip = dataset, machine_alias, hostname, pvn_ip
-    @internet_facing_ip, @memory_cap, @disk_cap, @cpu_cores, @copy_ssh_key =
-      internet_facing_ip, memory_cap, disk_cap, cpu_cores, copy_ssh_key
+    method(__method__).parameters.each{ |arg| eval("@#{arg[1]}=#{arg[1]}") }
   end
 
 end
